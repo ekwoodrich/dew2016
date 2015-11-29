316288fc7d7c8733442680daaa18b759fcd86f79
Eliot Kent Woodrich <eliot@woodrich.com>
2015-11-29 12:23:11 -0800

    Added view structure for elections, add styles for content.

--- server/DewHost/DewServer/views.py
+++ server/DewHost/DewServer/views.py
@@ -1,8 +1,43 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from DewServer import app
 
 @app.route('/')
 def index():    
-    return render_template('main.html')
+    return render_template('election_summary.html')
 
+
+@app.route('/elections/presidential/')
+def us_presidential():
+    
+    
+    return render_template('election_summary.html', page_title = "Republican Presidential Primary", page_icon = 'icon-gop')
+
+
+@app.route('/elections/presidential/<party>/')
+def us_presidential_party(party):
+    
+    if party == "republican":
+        page_icon = "icon-gop"
+        page_title = "Republican Presidential Primary"
+    elif party == "democratic":
+        page_icon = "icon-dem"
+        page_title = "Democratic Presidential Primary"
+    else:
+        return render_template('404.html'), 201
+
+    return render_template('election_summary.html', page_title = page_title, page_icon = page_icon)
+
+
+@app.route('/elections/senate/')
+def us_senate():    
+    return render_template('election_summary.html')
+
+
+@app.route('/elections/governor/')
+def us_governor():    
+    return render_template('election_summary.html')
+    
+@app.errorhandler(404)
+def not_found():
+    return render_template('404.html'), 201
--- server/external.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from urllib2 import urlopen
-
-
-
-def load_sunshine(update=True):
-    pass
