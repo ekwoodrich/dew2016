b3c59b04870057ef9da4eaf800e13f0cb0906902
Eliot Kent Woodrich <eliot@woodrich.com>
2015-12-09 23:16:14 -0800

    Expanded prototype context-free grammar poll parser.

--- server/DewHost/DewApi/pollparse/pyparse_test.py
+++ server/DewHost/DewApi/pollparse/pyparse_test.py
@@ -10,6 +10,7 @@ digits = Word(nums)
 months = Group('Jan') | Group('Feb') | Group('Mar') | Group('Apr') | Group('May') | Group('June') | Group('July') | Group('Aug') | Group('Sept') | Group('Oct') | Group('Nov') | Group('Dec')
 date_yr = Group(months + digits)
 page_num = Group("-" + digits + '-')
+string = '"' + Word(alphas) + '"'
 poll_screen = Group('(' + alphas  + ':)' )
 table_column = Group(date_yr + OneOrMore(Group(digits + '%')))
 
@@ -18,7 +19,7 @@ table_header = ')' + OneOrMore(Group(Word(alphas + nums + ':'   "'" + '?' + '"' 
 
 table = Group(OneOrMore(table_column))
 
-blocks   = table  | date_yr  |  Group(digits + '%')  | Group(digits + '.')  | Group(Word(alphas + nums + ':'   "'" + '?' + '"' + '/' + ' ' + '(' + ')' + ':' + ',' + '-' + '.')) | Group(digits) | '\r\n' 
+blocks   = table  | date_yr  |  Group(digits + '%')  | Group(Word(alphas + nums + ':'   "'" + '?' + '"' + '/' + ' ' + '(' + ')' + ':' + ',' + '-' + '.')) | Group(digits) | '\r\n' 
 group  = OneOrMore(blocks)
 
 grammar = group
