c7efd44af29986c27e09277749ca95fce2944cff
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2015-12-20 15:46:27 -0800

    Removed comments from config.

--- server/DewHost/config.py
+++ server/DewHost/config.py
@@ -15,10 +15,6 @@ DATABASE_CONNECT_OPTIONS = {}
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# Application threads. A common general assumption is
-# using 2 per available processor cores - to handle
-# incoming requests using one and performing background
-# operations using the other.
 THREADS_PER_PAGE = 2
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
